99148b062853bbf8beab06e22d9fb2ee14e65886
Tvick22 <trevbox1@outlook.com>
2024-04-18 13:19:41 -0700

    If timer doesn't active, warn

--- assets/js/platformer3x/GameControl.js
+++ assets/js/platformer3x/GameControl.js
@@ -100,6 +100,7 @@ const GameControl = {
      */
     startTimer() {
         if (GameEnv.timerActive) {
+            console.warn("TIMER ACTIVE: TRUE, TIMER NOT STARTED")
             return;
         }
         
@@ -165,7 +166,7 @@ const GameControl = {
         this.randomEventId = Math.floor(Math.random() * 3) + 1; //The number multiplied by Math.random() is the number of possible events.
         /**Random Event Key
          * 1: Inverts the Color of the Background
-         * 2: Time Stops all Goombas
+         * 2: Time Stops all Goombas    
          * 3: Kills a Random Goomba
         */
     },
