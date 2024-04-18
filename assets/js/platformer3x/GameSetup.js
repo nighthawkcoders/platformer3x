cca3a6dc9bc1c61c197be802bcb6ddfac333e7ee
gabhall <gabrielhall127@gmail.com>
2024-04-18 20:59:36 +0000

    change soil to ground

--- assets/js/platformer3x/GameSetup.js
+++ assets/js/platformer3x/GameSetup.js
@@ -185,7 +185,7 @@ const GameSetup = {
         alien: { src: "/images/platformer/platforms/alien.png" },
         bricks: { src: "/images/platformer/platforms/brick_wall.png" },
         sand: { src: "/images/platformer/platforms/sand.png"},
-        soil: { src: "/images/platfromer/platforms/soil.png" },
+        ground: { src: "/images/platformer/platforms/ground.png"},
         block: { src: "/images/platformer/platforms/brick_block.png" }, //MAY need 3 new variables: sizeRatio, widthRatio, and heightRatio
         itemBlock: { src: "/images/platformer/platforms/mario_block_spritesheet_v2.png",
           sizeRatio: 83.2,
@@ -538,7 +538,7 @@ const GameSetup = {
         const spaceGameObjects = [
           // GameObject(s), the order is important to z-index...
           { name: 'moon', id: 'background', class: Background, data: this.assets.backgrounds.moon },
-          { name: 'soil', id: 'platform', class: Platform, data: this.assets.platforms.soil },
+          { name: 'ground', id: 'platform', class: Platform, data: this.assets.platforms.ground },
           { name: 'blocks', id: 'jumpPlatform', class: BlockPlatform, data: this.assets.platforms.alien, xPercentage: 0.2, yPercentage: 0.85 },
           { name: 'blocks', id: 'jumpPlatform', class: BlockPlatform, data: this.assets.platforms.alien, xPercentage: 0.2368, yPercentage: 0.85 },
           { name: 'blocks', id: 'jumpPlatform', class: BlockPlatform, data: this.assets.platforms.alien, xPercentage: 0.5, yPercentage: 0.85 },
