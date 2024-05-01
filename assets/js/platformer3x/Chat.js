ecca166987533cf43a107b8e68a2190d005af956
AlexRubio1 <rubioalex688@gmail.com>
2024-05-01 14:23:57 -0700

    ewe

--- assets/js/platformer3x/Chat.js
+++ assets/js/platformer3x/Chat.js
@@ -1,7 +1,7 @@
 import Multiplayer from './Multiplayer.js'
 import createSound from './Sound.js';
 /**
- * Prevents players from typing no-no words in the chat.
+ * Prevents players from typing no-no words in the chqat.
  * If these words are detected, a pre-written message 
  * will be displayed instead
  */
@@ -13,7 +13,7 @@ class Chat {
         '911', 'die', 'luigi', 'peach', 'bowser', 'mario', 
         'mr.mortensen', 'mr. mortensen', 'mortensen', 'lopez', 
         'mr.lopez', 'mr. lopez','mister mortensen', 'mister lopez', 
-        'aws', 'amazonwebservices', 'amazon', 'amazonweb'];
+        'aws', 'amazonwebservices', 'amazon', 'amazonweb', 'shit', 'fuck', 'bitch', 'hoe', 'queer', 'gay', 'ass', 'asshole'];
 
         this.prohibitedWords.concat(wordsToAdd);
     }
@@ -29,7 +29,7 @@ class Chat {
     parseMessage(message){
         this.prohibitedWords.forEach(word => {
             const regex = new RegExp('\\b' + word + '\\b', 'gi');
-            message = message.replace(regex, 'I Love CSSE! '.repeat(word.length));
+            message = message.replace(regex, 'This is the Central Intelligentsia of the Chinese Communist Party. 您的 Internet 浏览器历史记录和活动引起了我们的注意 YOUR INTERNET ACTIVITY HAS ATTRACTED OUR ATTENTION. 志們注意了 you have been found protesting in the subreddit!!!!! 這是通知你，你必須 我們將接管台灣 serious crime 以及世界其他地方 100 social credits have been deducted from your account 這對我們所有未來的下屬來說都是一個重要的機會 stop the protest immediately 立即加入我們的宣傳活動，提前獲得 do not do this again! 不要再这样做! if you do not hesitate, more social credits ( -11115 social credits )will be subtracted from your profile, resulting in the subtraction of ration supplies. (由人民供应部重新分配 ccp) you will also be sent into a re-education camp in the xinjiang uyghur autonomous zone. 为党争光! Glory to the CCP! 中华人民共和国国家安全部紧急通告'.repeat(word.length));
         });
         return message;
     }
